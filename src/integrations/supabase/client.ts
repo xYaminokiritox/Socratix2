ad66052d91d002d4248f344cd46c0d7723385e6b
xYaminokiritox <aryansaini2004feb@gmail.com>
2025-05-14 20:58:28 +0530

    env added

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,9 +2,12 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://bhirorpogkfnafuxfkox.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoaXJvcnBvZ2tmbmFmdXhma294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5ODA2NDMsImV4cCI6MjA2MjU1NjY0M30.0c2AKpiQQuyCU7-QkvpXpu1rm1Ex06x4gAiH3BX9l4U";
-
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+if (!SUPABASE_URL || !SUPABASE_PUBLISHABLE_KEY) {
+    throw new Error("Missing Supabase environment variables.");
+  }
+  
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
